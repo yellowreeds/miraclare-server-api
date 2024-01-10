d4566de5340d7970546f2b2811caba4a90be0d0d
abismaw <yellowreeds@gmail.com>
2024-01-10 13:56:09 +0900

    fix averaging3

--- server.js
+++ server.js
@@ -599,13 +599,13 @@ app.post('/api/customers/sleepDataResult', upload.fields([
                   result.br_data = brData;
           
                   // Step 5: Calculate the average
-                  if (results3.length > 0) {
+                  if (br_data.length > 0) {
                     let totalSum = 0;
-                    results3.forEach((row) => {
+                    br_data.forEach((row) => {
                       totalSum += row.sleep_br_episode || 0;
 
                     });
-                    result.average_br_episode = totalSum / results3.length;
+                    result.average_br_episode = totalSum / br_data.length;
                   }
           
                   res.status(200).json(result);
